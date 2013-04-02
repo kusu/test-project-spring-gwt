151a4c9fbe1ef1bf1f17eb7bd65de32b2a97c580
pr.kusu <pr.kusu@gmail.com>
2013-04-02 17:21:27 +0545

    add test context

--- test-project/src/test/java/com/me/testproject/test/AbstractTestCase.java
+++ test-project/src/test/java/com/me/testproject/test/AbstractTestCase.java
@@ -1,10 +1,12 @@
 package com.me.testproject.test;
 
 import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.AbstractTransactionalJUnit4SpringContextTests;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration({ "classpath:WEB-INF/test-context.xml" })
 public abstract class AbstractTestCase  extends AbstractTransactionalJUnit4SpringContextTests {
 
 }
