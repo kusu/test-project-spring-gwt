9ac67d1cb37b42586de12cb38de620eefcafec45
kusu <kusu@192.168.10.4>
2013-04-08 18:46:42 +0545

    clean code

--- test-project/src/test/java/com/me/testproject/test/UserApiTest.java
+++ test-project/src/test/java/com/me/testproject/test/UserApiTest.java
@@ -7,7 +7,6 @@ import java.util.List;
 import junit.framework.Assert;
 
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
